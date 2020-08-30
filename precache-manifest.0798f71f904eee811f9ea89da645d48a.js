a1ad361f81fcc5311d084821b14d3598e620a860
Mehul Kothari <34826782+mehulk05@users.noreply.github.com>
2020-08-30 09:17:20 +0530

    Updates

---
+++ precache-manifest.0798f71f904eee811f9ea89da645d48a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c582c786b6f0525bd0cce5230e66f307",
+    "url": "/Medium-Blog-On-React-App/index.html"
+  },
+  {
+    "revision": "cfae4bfedeacd094305b",
+    "url": "/Medium-Blog-On-React-App/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ce0ec78f05b92119d5cc",
+    "url": "/Medium-Blog-On-React-App/static/css/main.397d573c.chunk.css"
+  },
+  {
+    "revision": "cfae4bfedeacd094305b",
+    "url": "/Medium-Blog-On-React-App/static/js/2.38920845.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/Medium-Blog-On-React-App/static/js/2.38920845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0ec78f05b92119d5cc",
+    "url": "/Medium-Blog-On-React-App/static/js/main.115699b4.chunk.js"
+  },
+  {
+    "revision": "5d2b78a955f59a39e5e7",
+    "url": "/Medium-Blog-On-React-App/static/js/runtime-main.980a8f34.js"
+  }
+]);
